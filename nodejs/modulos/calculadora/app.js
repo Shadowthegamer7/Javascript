51c745790641bb5c4998ff63c1d7aa3220444dcb
arthuroliveiramoraes <athurtorresmoraes@gmail.com>
2025-08-26 09:29:54 -0300

    Versão calculadora

---
+++ nodejs/modulos/calculadora/app.js
@@ -0,0 +1,6 @@
+const funcoesMatematicas = require('./funcoes_matematicas');
+
+console.log('Soma: ', funcoesMatematicas.soma(10, 5));
+console.log('Subtração: ', funcoesMatematicas.subtrai(10, 5));
+console.log('Multiplicação: ', funcoesMatematicas.multiplica(10, 5));
+console.log('Divisão: ', funcoesMatematicas.divide(10, 5));
